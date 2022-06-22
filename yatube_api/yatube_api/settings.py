a63cebcbd4d1c60848a6cbd1254c18ec6e6a8045
Николай Каштанов <kersam19@yandex.ru>
2022-06-22 14:15:47 +0300

    22/06/22_1 Тесты пройдены

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -98,6 +98,6 @@ REST_FRAMEWORK = {
 }
 SIMPLE_JWT = {
     # Устанавливаем срок жизни токена
-   'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
-   'AUTH_HEADER_TYPES': ('Bearer',),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'AUTH_HEADER_TYPES': ('Bearer',),
 }
