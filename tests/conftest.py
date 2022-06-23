3d4580298572930799068d468c13b82b796d10c8
Николай Каштанов <kersam19@yandex.ru>
2022-06-23 23:33:40 +0300

    23/06/22_1 почти все правки

--- tests/conftest.py
+++ tests/conftest.py
@@ -40,8 +40,8 @@ assert filename in root_dir_content, (
     f'В корне проекта не найден файл `{filename}`'
 )
 
-#with open(filename, 'r') as f:
-#    file = f.read()
-#    assert file != default_md, (
-#        f'Не забудьте оформить `{filename}`'
-#    )
+with open(filename, 'r') as f:
+    file = f.read()
+    assert file != default_md, (
+        f'Не забудьте оформить `{filename}`'
+    )
