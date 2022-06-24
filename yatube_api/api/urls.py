e5836a4e69c286b3baf6b457ff74c8e36db3aa8c
Nick <kersam19@yandex.ru>
2022-06-24 17:00:56 +0300

    Все поправил

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -13,9 +13,15 @@ v1_router.register('groups', GroupViewSet, basename='groups')
 v1_router.register('follow', FollowViewSet, basename='follow')
 
 jwtpatterns = [
-    re_path(r'^create/?', views.TokenObtainPairView.as_view(), name='jwt-create'),
-    re_path(r'^refresh/?', views.TokenRefreshView.as_view(), name='jwt-refresh'),
-    re_path(r'^verify/?', views.TokenVerifyView.as_view(), name='jwt-verify'),
+    re_path(
+        r'^create/?', views.TokenObtainPairView.as_view(), name='jwt-create'
+        ),
+    re_path(
+        r'^refresh/?', views.TokenRefreshView.as_view(), name='jwt-refresh'
+        ),
+    re_path(
+        r'^verify/?', views.TokenVerifyView.as_view(), name='jwt-verify'
+        ),
 ]
 
 urlpatterns = [
